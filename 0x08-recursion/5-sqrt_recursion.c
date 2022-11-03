7aa0d033cc423c8573a9bbf0f9746e3d29e85f50
Budaboss <opiyocaroline20@gmail.com>
2022-11-03 14:59:00 -0700

    commit task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,7 +4,6 @@ int actual_sqrt_recursion(int n, int i);
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- *
  * @n: number to calculate the square root of
  *
  * Return: the resulting square root
@@ -28,4 +27,7 @@ int actual_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
 }
