601c04082179db4d78d01c1494779c31d1acda77
Budaboss <opiyocaroline20@gmail.com>
2022-11-07 15:23:49 -0800

    commit task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,28 +2,19 @@
 #include <stdlib.h>
 
 /**
- * main - enter point
- * @argc: number of args
- * @argv: holds args
- * Return 1 for no arg
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int a;
-	int b;
-	int re;
-
-	if (argc <= 2)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		re = a * b;
-		printf("%d\n", re);
-	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
+
